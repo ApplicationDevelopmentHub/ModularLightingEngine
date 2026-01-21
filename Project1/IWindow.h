b636cd45ef5ea046808695d92b944b16bd6de107
AnupamChauhans <anupamchauhan504@gmail.com>
2026-01-21 08:30:37 +0530

    Added GLEW and GLFW libraries
    Added GLEW and GLFW libraries

--- Project1/IWindow.h
+++ Project1/IWindow.h
@@ -1,5 +1,11 @@
 #pragma once
 class IWindow {
 public:
+	virtual ~IWindow() = default;
+	virtual bool Create(int width,int height, const char* title)=0;
+	virtual void PollEvents() = 0;
+	virtual void SwapBuffers() = 0;
+	virtual bool ShouldClose() = 0;
+	virtual void* GetNativeHandle() const = 0;
 private:
 };
